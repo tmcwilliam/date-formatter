01c1510554400ccca28dbd8dc240745a6925a276
Travis McWilliam <t.mcwilliam@gmail.com>
2017-02-09 14:25:42 -0600

    Update Babel. Move to Tape

--- src/index.js
+++ src/index.js
@@ -1,109 +1,102 @@
 import moment from 'moment';
-import _ from 'lodash';
 
 const DATE_FORMATS = {
-		'micro': 'M/D/YYYY',
-		'tiny': 'MM/DD/YYYY',
-		'short': 'MMM DD, YYYY',
-		'shorter': 'MMM D, YYYY',
-		'medium': 'ddd, MMM DD, YYYY',
-		'long': 'MMMM DD, YYYY',
-		'full': 'dddd, MMMM DD, YYYY',
-		'picker': 'MMM YYYY',
-		'monthday': 'MMM DD',
-		'monthyear': 'MMMM YYYY',
-		'jqTiny': 'mm/dd/yy',
-		'dayofweek': 'dddd',
-		'year': 'YYYY'
-	};
+	'micro': 'M/D/YYYY',
+	'tiny': 'MM/DD/YYYY',
+	'short': 'MMM DD, YYYY',
+	'shorter': 'MMM D, YYYY',
+	'medium': 'ddd, MMM DD, YYYY',
+	'long': 'MMMM DD, YYYY',
+	'full': 'dddd, MMMM DD, YYYY',
+	'picker': 'MMM YYYY',
+	'monthday': 'MMM DD',
+	'monthyear': 'MMMM YYYY',
+	'jqTiny': 'mm/dd/yy',
+	'dayofweek': 'dddd',
+	'year': 'YYYY'
+};
 
-/**	@method _validateDate
-
- Shared utility to ensure a valid date is sent. If a bad date is sent, throw a warning and return the current date
+/**	@method validate
+ * 
  @private
- @param {Date | Object} dateObj Javascript date object or Ss.Timezone object
+ @param {Date} - Javascript date object
  @return {Date}
  */
-const _validateDate = (dateObj) => {
-	// Ss.Timezone.localDate returns an object - not a date - so extract the right field
-	if (dateObj.utc && _.isDate(dateObj.utc)) {
-		dateObj = dateObj.utc;
+const validate = (date) => {
+	if (!(date instanceof Date)) {
+		return `Invalid date passed to dateTimeFormatter. (${date})`;
+
+		date = new Date();
 	}
 
-	if (!_.isDate(dateObj)) {
-		console.error(`Invalid date passed to dateTimeFormatter. (${dateObj})`);
-
-		dateObj = new Date();
-	}
-
-	return dateObj;
+	return date;
 };
 /**
  @method dateTimeFormatter
  @private
  @param {Date} date
- @param {Object} options
+ @param {String} pattern
  @return {String} Returns a formatted datetime string
  */
-const dateTimeFormatter = (date, options = {}) => {
-	date = _validateDate(date);
+const dateTimeFormatter = (date, pattern) => {
+	date = validate(date);
 
-	return moment(date).format(options.pattern);
+	return moment(date).format(pattern);
 };
 
 // 1/2/2015 (no leading zero)
 const toMicroDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.micro });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.micro);
 };
 
 // 11/24/2011, 01/02/2015
 const toTinyDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.tiny });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.tiny);
 };
 
 // Nov 4, 2011
 const toShorterDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.shorter });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.shorter);
 };
 
 // Nov 04, 2011
 const toShortDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS['short'] });
+	return dateTimeFormatter(dateObj, DATE_FORMATS['short']);
 };
 
 // Aug 25
 const toMonthDayDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.monthday });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.monthday);
 };
 
 // Thu, Nov 24, 2011
 const toMediumDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.medium });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.medium);
 };
 
 // November 24, 2011
 const toLongDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS['long'] });
+	return dateTimeFormatter(dateObj, DATE_FORMATS['long']);
 };
 
 // Thursday, November 24, 2011
 const toFullDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.full });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.full);
 };
 
 // Nov 2011
 const toDatepicker = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.picker });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.picker);
 };
 
 // November 2011
 const toMonthYearDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: DATE_FORMATS.monthyear });
+	return dateTimeFormatter(dateObj, DATE_FORMATS.monthyear);
 };
 
 // 2011-11-24 (for the datetime attribute of a <time> element)
 const toYearMonthDate = (dateObj) => {
-	return dateTimeFormatter(dateObj, { pattern: 'YYYY-MM-DD' });
+	return dateTimeFormatter(dateObj, 'YYYY-MM-DD');
 };
 
 export { 
---
+++ test.js
@@ -0,0 +1,24 @@
+import { test } from 'tape'
+import { 
+	toMicroDate, toTinyDate, toShorterDate, toShortDate, toMonthDayDate,
+	toMediumDate, toLongDate, toFullDate, toDatepicker, toMonthYearDate, toYearMonthDate 
+} from './src/index';
+
+const AUGUST_9_2015 = new Date(1439096900000)
+
+test('Date Formatter', (assert) => {
+	assert.equal(toMicroDate(AUGUST_9_2015), '8/9/2015')
+	assert.equal(toMicroDate(AUGUST_9_2015), '8/9/2015')
+	assert.equal(toTinyDate(AUGUST_9_2015), '08/09/2015')
+	assert.equal(toShorterDate(AUGUST_9_2015), 'Aug 9, 2015')
+	assert.equal(toShortDate(AUGUST_9_2015), 'Aug 09, 2015')
+	assert.equal(toMonthDayDate(AUGUST_9_2015), 'Aug 09')
+	assert.equal(toMediumDate(AUGUST_9_2015), 'Sun, Aug 09, 2015')
+	assert.equal(toLongDate(AUGUST_9_2015), 'August 09, 2015')
+	assert.equal(toFullDate(AUGUST_9_2015), 'Sunday, August 09, 2015')
+	assert.equal(toDatepicker(AUGUST_9_2015), 'Aug 2015')
+	assert.equal(toMonthYearDate(AUGUST_9_2015), 'August 2015')
+	assert.equal(toYearMonthDate(AUGUST_9_2015), '2015-08-09')
+
+	assert.end()
+})
